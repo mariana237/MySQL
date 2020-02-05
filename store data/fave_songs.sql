041ba0a15189ae77828554442795afa2fc7006d8
mariana237 <60699422+mariana237@users.noreply.github.com>
2020-02-05 21:32:53 +0700

    Add files via upload

---
+++ store data/fave_songs.sql
@@ -0,0 +1,12 @@
+CREATE TABLE fave_songs (
+				song INT PRIMARY KEY,
+                song_title VARCHAR(40),
+                singer VARCHAR(40),
+                song_year VARCHAR(40)
+                );           
+                
+INSERT INTO fave_songs VALUES (1,"Love Will Set You Free","Kodaline",2015);
+INSERT INTO fave_songs VALUES (2,"Autopilot","Kodaline",2014);
+INSERT INTO fave_songs VALUES (3,"Imagination","Shawn Mendes",2015);
+INSERT INTO fave_songs VALUES (4,"Between The Bars","Ellioth Smith",1997);
+INSERT INTO fave_songs VALUES (5,"Moving On","Kodaline",2017)
